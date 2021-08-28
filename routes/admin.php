c353042365e6c8e496d7ff11d334f59ee4b05ba5
fajar sidik priatna <jardik.oryza@gmail.com>
2021-08-28 15:15:24 +0700

    add forum model & migration

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['isAdmin', 'auth'], 'prefix' => 'admin', 'as' => 'admin'], function () {
 	Route::get('dashboard', [DashboardController::class, 'index'])->name('.index');
+
+	Route::group(['prefix' => 'categories', 'as' => '.categories'], function () {
+		Route::get('/', [CategoryController::class, 'index'])->name('.index');
+
+		Route::get('create', [CategoryController::class, 'create'])->name('.create');
+		Route::post('/', [CategoryController::class, 'store'])->name('.store');
+
+		Route::get('edit/{category:slug}', [CategoryController::class, 'edit'])->name('.edit');
+		Route::put('update/{category:slug}', [CategoryController::class, 'update'])->name('.update');
+
+		Route::get('{category:slug}', [CategoryController::class, 'show'])->name('.show');
+		Route::delete('{category:slug}', [CategoryController::class, 'destroy'])->name('.destroy');
+	});
 });
